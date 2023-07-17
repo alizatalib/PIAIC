a5d9e931a1060d4177f406a3c0ca9e1ce41b03ec
alizatalib <139770357+alizatalib@users.noreply.github.com>
2023-07-17 17:42:45 +0500

    Question 4
    Question 4

---
+++ Q4.js
@@ -0,0 +1,4 @@
+var quote = "Be yourself; everyone else is already taken.";
+var author = "Oscar Wilde";
+console.log("\"".concat(quote, "\""));
+console.log("- ".concat(author));
---
+++ Q4.ts
@@ -0,0 +1,5 @@
+const quote: string = "Be yourself; everyone else is already taken.";
+const author: string = "Oscar Wilde";
+
+console.log(`"${quote}"`);
+console.log(`- ${author}`);
